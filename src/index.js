defb33299172e388e88ee5417762cf88d87315c0
BeCr34t1v3Jas <jazmincan@hotmail.de>
2026-01-13 06:46:34 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import React from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
-import "./styles.css";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
-root.render(<App />);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
