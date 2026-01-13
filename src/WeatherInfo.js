6c8489e292cfb5875219d4b159d39870b2bc944b
BeCr34t1v3Jas <jazmincan@hotmail.de>
2026-01-13 06:20:29 +0100

    Update WeatherInfo.js
    changed the structure to fulfill the tasks

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,40 +1,29 @@
 import React from "react";
 
-export default function WeatherInfo({ data }) {
+export default function WeatherInfo(props) {
   return (
     <div className="weather-card">
-      <div className="weather-main">
+      <div className="weather-header">
         <div>
-          <h1>{data.city}</h1>
+          <h1>{props.city}</h1>
           <ul>
-            <li>
-              {data.date.toLocaleString("en-US", {
-                weekday: "long",
-                hour: "2-digit",
-                minute: "2-digit"
-              })}
-            </li>
-            <li className="text-capitalize">{data.description}</li>
+            <li>{props.description}</li>
           </ul>
         </div>
 
         <div className="weather-details">
           <ul>
-            <li>Humidity: <strong>{data.humidity}%</strong></li>
-            <li>Wind: <strong>{Math.round(data.wind)} km/h</strong></li>
+            <li>Humidity: <strong>{props.humidity}%</strong></li>
+            <li>Wind: <strong>{props.wind} km/h</strong></li>
           </ul>
         </div>
       </div>
 
       <div className="weather-temperature">
-        <img
-          src={`https://openweathermap.org/img/wn/${data.icon}@2x.png`}
-          alt={data.description}
-        />
-        <span className="temp">{data.temperature}</span>
+        <img src={props.icon} alt={props.description} />
+        <span className="temp">{props.temperature}</span>
         <span className="unit">°C</span>
       </div>
     </div>
   );
 }
-
