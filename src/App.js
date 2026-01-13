d68cb9016a8bdaae5aabd856583fbd3edef428c1
BeCr34t1v3Jas <jazmincan@hotmail.de>
2026-01-13 07:29:57 +0100

    Update App.js
    include the right spelling of the code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-mport React from "react";
+import React from "react";
 import Weather from "./Weather";
 import "./App.css";
 
@@ -22,3 +22,4 @@ export default function App() {
     </div>
   );
 }
+
